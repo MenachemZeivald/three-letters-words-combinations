7dbdd4826810ecf3de128f6bcb8f2548559b08b3
Menachem <moshe456tv@gmail.com>
2022-06-27 13:32:51 +0300

    adding sum and average

--- three_letters.py
+++ three_letters.py
@@ -1,26 +1,24 @@
 import csv
+from timeit import default_timer
 
 alphabet = "אבגדהוזחטיכלמנסעפצקרשת"
-words_list = []
-
-
-def csv_to_lst():
-    with open("hebrew_words_three_letters.csv", encoding="utf-8-sig") as words_file:
-        file = csv.reader(words_file)
-        for line in file:
-            words_list.append(line[0])
+words_list = {}
 
 
 def word_checker():
+    with open("hebrew_words_three_letters.csv", encoding="utf-8-sig") as words_file:
+        file = csv.reader(words_file)
+        for line in file:
+            words_list[line[0]] = int(line[1])
     with open("letters.csv", "w", encoding="utf-8-sig", newline="") as letters_file:
         writer = csv.writer(letters_file)
-        writer.writerow(["word", "count", "words"])
+        writer.writerow(["word", "count", "words", "score", "sum", "average"])
         already_in = []
         for a in alphabet:
             for b in alphabet:
                 for c in alphabet:
                     count = 0
-                    situation = []
+                    situation = {}
                     num = [
                         a + b + c,
                         a + c + b,
@@ -33,12 +31,23 @@ def word_checker():
                         if n in words_list and n not in already_in:
                             count += 1
                             already_in.append(n)
-                            situation.append(n)
+                            situation[n] = words_list[n]
                     if count > 4:
-                        writer.writerow([a + b + c, count, situation])
+                        writer.writerow(
+                            [
+                                a + b + c,
+                                count,
+                                ", ".join([i for i in situation]),
+                                ", ".join([str(i) for i in situation.values()]),
+                                sum(situation.values()),
+                                (sum(situation.values())) // 6,
+                            ]
+                        )
 
     return True
 
 
-csv_to_lst()
+start = default_timer()
 word_checker()
+end = default_timer()
+print(f"the time is {end-start}")
